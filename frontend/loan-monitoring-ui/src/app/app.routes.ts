b626d53272414e6dea1b9e6ab7d13d8ab4d3aba3
Santhosh Kumar Reddy Marri <santhoshkumarreddy423@gmail.com>
2026-07-13 09:13:03 +0530

    Implimented Angulars first Component i.e Login Component

--- frontend/loan-monitoring-ui/src/app/app.routes.ts
+++ frontend/loan-monitoring-ui/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './features/auth/pages/login/login';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Login,
+  },
+];
